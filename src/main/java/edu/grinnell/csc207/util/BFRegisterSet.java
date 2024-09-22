7e299acf9425998ace49b8cafccb6c1c15d61729
Ryanalys <ryanalys@grinnell.edu>
2024-09-22 00:56:38 -0500

    BFRegisterSet updates

--- src/main/java/edu/grinnell/csc207/util/BFRegisterSet.java
+++ src/main/java/edu/grinnell/csc207/util/BFRegisterSet.java
@@ -1,5 +1,14 @@
 package edu.grinnell.csc207.util;
 
 public class BFRegisterSet {
-  
+  public BigFraction[] reg = new BigFraction[26];
+
+  public BFRegisterSet(BigFraction[] set){
+    this.reg = set;
+  }
+
+  public void store(char letter, BigFraction val){
+    int index = (int) letter - (int) 'a';
+    reg[index] = val;
+  }
 }
